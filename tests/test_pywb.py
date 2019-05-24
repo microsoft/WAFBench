7ac9890f219a13988934705298e5be4de3fe7c86
zegan <zegan@microsoft.com>
2019-05-24 13:23:59 +0800

    [enhance]: Add test for pywb

---
+++ tests/test_pywb.py
@@ -0,0 +1,120 @@
+import BaseHTTPServer
+import threading
+import time
+import os
+import base64
+
+from pywb import main
+
+import common
+
+
+def test_send_packet_specified_number():
+    counter = {
+        "request" : 0
+    }
+    class HTTPHandler(BaseHTTPServer.BaseHTTPRequestHandler):
+        def do_GET(self):
+            self.send_response(200)
+            counter["request"] += 1
+    with common.HTTPServerInstance(HTTPHandler):
+        expect_request_count = 3
+        main.execute(["-v", "4", "-n", str(expect_request_count),
+                    "localhost:" + str(common._PORT)])
+        assert(counter["request"] == expect_request_count)
+
+
+def test_send_packet_specified_timelimit():
+    with common.HTTPServerInstance():
+        expect_request_time = 2
+        start_time = time.time()
+        main.execute(["-v", "4", "-t", str(expect_request_time),
+                    "localhost:" + str(common._PORT)])
+        assert(abs((time.time() - start_time) - expect_request_time) < 0.5)
+
+
+def test_post_file():
+    counter = {
+        "request" : 0,
+    }
+    expect_content_type = ""
+    class HTTPHandler(BaseHTTPServer.BaseHTTPRequestHandler):
+        def do_POST(self):
+            self.send_response(200)
+            assert(self.headers.get("Content-Type") == expect_content_type)
+            counter["request"] += 1
+    target_file = os.path.join(os.path.dirname(
+        __file__), "data", "requestbody2kb.json")
+    with common.HTTPServerInstance(HTTPHandler):
+        expect_content_type = "wafbench-test"
+        main.execute(["-v", "4", "-p", target_file,  "-n", "1", "-T", expect_content_type,
+                    "localhost:" + str(common._PORT)])
+        assert(counter["request"] == 1)
+
+        expect_content_type = "application/json"
+        main.execute(["-v", "4", "-p", target_file,  "-n", "1",
+                    "localhost:" + str(common._PORT)])
+        assert(counter["request"] == 2)
+
+
+def test_header_check():
+    counter = {
+        "request" : 0,
+    }
+    expect_header = ()
+    class HTTPHandler(BaseHTTPServer.BaseHTTPRequestHandler):
+        def do_GET(self):
+            self.send_response(200)
+            if expect_header:
+                assert(self.headers.get(expect_header[0]) == expect_header[1])
+            counter["request"] += 1
+        def do_HEAD(self):
+            self.send_response(200)
+            counter["request"] += 1
+
+    with common.HTTPServerInstance(HTTPHandler):
+
+        main.execute(["-v", "4", "-i", "-n", "1",
+                    "localhost:" + str(common._PORT)])
+        assert(counter["request"] == 1)
+
+        expect_header = ("Host", "wafbench")
+        main.execute(["-v", "5", "-H", "%s: %s" % expect_header, "-n", "1",
+                    "localhost:" + str(common._PORT)])
+        assert(counter["request"] == 2)
+
+        expect_header = ("Connection", "wafbench")
+        main.execute(["-v", "5", "-H", "%s: %s" % expect_header, "-n", "1",
+                    "localhost:" + str(common._PORT)])
+        assert(counter["request"] == 3)
+
+        expect_header = ("Cookie", "wafbench=wafbench")
+        main.execute(["-v", "5", "-C", expect_header[1], "-n", "1",
+                    "localhost:" + str(common._PORT)])
+        assert(counter["request"] == 4)
+
+        authorization = "wafbench:wafbench"
+        expect_header = ("Authorization", "Basic " +
+                        base64.b64encode(authorization))
+        main.execute(["-v", "5", "-A", authorization, "-n", "1",
+                    "localhost:" + str(common._PORT)])
+        assert(counter["request"] == 5)
+
+        authorization = "wafbench:wafbench"
+        expect_header = ("Proxy-Authorization", "Basic " +
+                        base64.b64encode(authorization))
+        main.execute(["-v", "5", "-P", authorization, "-n", "1",
+                    "localhost:" + str(common._PORT)])
+        assert(counter["request"] == 6)
+
+        expect_header = ("Connection", "Keep-Alive")
+        main.execute(["-v", "5", "-k", "-n", "1",
+                    "localhost:" + str(common._PORT)])
+        assert(counter["request"] == 7)
+
+        expect_header = ("Connection", "Close")
+        main.execute(["-v", "5", "-2", "2", "-n", "1",
+                    "localhost:" + str(common._PORT)])
+        assert(counter["request"] == 8)
+
+
